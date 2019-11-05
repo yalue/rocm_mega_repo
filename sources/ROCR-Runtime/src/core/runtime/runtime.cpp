93c8518780c3761304d0ce4499a2a925f9b0a3b6
Nathan O <Nathan O>
2019-11-05 14:10:41 -0500

    Don't load hsa-ext-image64 or hsa-ext-finalize64
     - This change simply comments out the loading of two closed-source
       shared libraries by the HSA runtime. I don't know the correct way to
       get them, and they aren't included when building ROCm from source, so
       I'm commenting out their inclusion for now. This basically prevents a
       missing-library error message that was previously appearing
       every time I ran any ROCm program.

--- sources/ROCR-Runtime/src/core/runtime/runtime.cpp
+++ sources/ROCR-Runtime/src/core/runtime/runtime.cpp
@@ -1329,6 +1329,11 @@ void Runtime::LoadExtensions() {
                                           "libhsa-ext-image.so.1"};
 #endif
 
+  /* Commenting this out to avoid the messages about missing libraries when
+   * running any ROCm application. I may uncomment it when I find a source for
+   * these (closed-source and deprecated) libraries that can't be built from
+   * source.
+
   // Update Hsa Api Table with handle of Image extension Apis
   extensions_.LoadFinalizer(kFinalizerLib[os_index(os::current_os)]);
   hsa_api_table_.LinkExts(&extensions_.finalizer_api,
@@ -1338,6 +1343,7 @@ void Runtime::LoadExtensions() {
   extensions_.LoadImage(kImageLib[os_index(os::current_os)]);
   hsa_api_table_.LinkExts(&extensions_.image_api,
                           core::HsaApiTable::HSA_EXT_IMAGE_API_TABLE_ID);
+  */
 }
 
 void Runtime::UnloadExtensions() { extensions_.Unload(); }
